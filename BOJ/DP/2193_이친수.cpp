a42f1ef0881e122a2098380f33a932ad6680a322
dldlstjq <inseob135@gmail.com>
2021-01-10 23:24:04 +0900

    boj

--- BOJ/DP/2193_이친수.cpp
+++ BOJ/DP/2193_이친수.cpp
@@ -31,3 +31,51 @@ int main() {
 	cout << sum << '\n';
 	return 0;
 }
+
+
+/////////////////////////////////////////////////
+/*
+재귀함수로 푼 풀이.
+num은 n자리, digit은 첫 자리의 수.
+digit이 0이면 0과 1 둘다 올 수 있고 1이면 0밖에 오지 못한다.
+그걸 이용해서 점화식을 세우면 정답이다.
+int형이 아닌 long long형을 써야 정답을 맞을 수 있다.
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int n;
+long long cache[91][2];
+
+long long pinary(int num, int digit) {
+	//한 자리 이친수는 1.
+	if (num == 1)
+		return 1;
+	long long& ret = cache[num][digit];
+	if (ret != -1)return ret;
+	ret = 0;
+	//digit이 1이면 0만 올 수 있다.
+	if (digit == 1)
+		ret += pinary(num - 1, 0);
+	//0이면 0,1 둘 다 올 수 있다.
+	else {
+		ret += pinary(num - 1, 0);
+		ret += pinary(num - 1, 1);
+	}
+
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	memset(cache, -1, sizeof(cache));
+	cin >> n;
+	cout << pinary(n, 1);
+
+	return 0;
+}
