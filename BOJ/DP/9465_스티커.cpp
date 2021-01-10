1d935cd241a296dcfe395704b9d07e873da2005b
dldlstjq <inseob135@gmail.com>
2021-01-10 17:38:29 +0900

    boj

--- BOJ/DP/9465_스티커.cpp
+++ BOJ/DP/9465_스티커.cpp
@@ -46,3 +46,63 @@ int main() {
 
     return 0;
 }
+
+
+
+///////////////////////////////////////
+/*재귀함수로 푼 풀이. 위의 방식대로 푼 것보다 메모리와 시간이 더 들었다.
+종만북에서 설명한 대로 계산은 조금 느리게 나왔다.
+*/
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int cache[2][100001];
+int sticker[2][100001];
+int n;
+
+int solve(int y, int x) {
+	//기저사례: x가 끝에 오면 해당 점수 반환
+	if (x == n - 1)
+		return sticker[y][x];
+	int& ret = cache[y][x];
+	if (ret != -1)return ret;
+	ret = 0;
+	//첫째 줄일때는 둘째 줄로 이동
+	if (y == 0) {
+		if (x + 1 <= n - 1)
+			ret = max(ret, solve(y + 1, x + 1) + sticker[y][x]);
+		if (x + 2 <= n - 1)
+			ret = max(ret, solve(y + 1, x + 2) + sticker[y][x]);
+	}
+	//둘째 줄일때는 첫째 줄로 이동
+	else {
+		if (x + 1 <= n - 1)
+			ret = max(ret, solve(y - 1, x + 1) + sticker[y][x]);
+		if (x + 2 <= n - 1)
+			ret = max(ret, solve(y - 1, x + 2) + sticker[y][x]);
+	}
+	return ret;
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int c;
+	cin >> c;
+	while (c--) {
+		memset(cache, -1, sizeof(cache));
+
+		cin >> n;
+		for (int i = 0; i < 2; ++i)
+			for (int j = 0; j < n; ++j)
+				cin >> sticker[i][j];
+		//(0,0), (1,0) 둘 중 점수 더 높은 쪽이 최대값
+		cout << max(solve(0, 0), solve(1, 0)) << "\n";
+	}
+
+	return 0;
+}
