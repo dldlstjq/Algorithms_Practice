b9457274256a386b80eeac86077a94e7e6e50344
dldlstjq <inseob135@gmail.com>
2021-01-12 12:55:27 +0900

    boj

--- BOJ/DP/11055_가장 큰 증가 부분 수열.cpp
+++ BOJ/DP/11055_가장 큰 증가 부분 수열.cpp
@@ -5,6 +5,26 @@
 
 using namespace std;
 
+//재귀함수로 푼 풀이. 기본적인 알고리즘은 반복문으로 푼 것과
+//비슷하고, ret을 해당 인덱스의 값으로 잡아준다.
+//그리고 다음 인덱스에 해당하는 값이 더 크면 그 값을 더해준다.
+//메인 함수에서 실행할 때 배열의 모든 인덱스를 순회하면서 실행해야
+//가장 큰 값을 구할 수 있다.
+int n;
+int A[1001];
+int cache[1001];
+
+int lis(int start) {
+    int& ret = cache[start];
+    if (ret != -1)return ret;
+    ret = A[start];
+    for (int i = start + 1; i < n; ++i)
+        if (A[start] < A[i])
+            ret = max(ret, lis(i) + A[start]);
+    return ret;
+}
+
+
 int dp[1001];
 
 int maximum(int a, int b) {
