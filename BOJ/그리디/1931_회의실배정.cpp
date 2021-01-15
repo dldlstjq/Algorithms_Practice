01bfaabca4a0ca2d00ce04e8e52f55798e55661c
dldlstjq <inseob135@gmail.com>
2021-01-15 16:02:36 +0900

    boj

--- BOJ/그리디/1931_회의실배정.cpp
+++ BOJ/그리디/1931_회의실배정.cpp
@@ -16,6 +16,29 @@
 
 using namespace std;
 
+//종만북 회의실 알고리즘
+//각 회의는 [begin,end) 구간 동안 회의실을 사용한다.
+int n;
+int start[100001], finish[100001];
+int schedule() {
+    //일찍 끝나는 순서대로 정렬한다.
+    vector<pair<int, int>> order;
+    for (int i = 0; i < n; ++i)
+        order.push_back(make_pair(finish[i], start[i]));
+    sort(order.begin(), order.end());
+    //earliest: 다음 회의가 시작할 수 있는 가장 빠른 시간
+    //selected: 지금까지 선택한 회의의 수
+    int earliest = 0, selected = 0;
+    for (int i = 0; i < order.size(); ++i) {
+        int meetingBegin = order[i].second, meetingEnd = order[i].first;
+        if (earliest <= meetingBegin) {
+            //earliest를 마지막 회의가 끝난 시간 이후로 갱신한다.
+            earliest = meetingEnd;
+            ++selected;
+        }
+    }
+    return selected;
+}
 
 bool cmp(const pair<int, int>& a, const pair<int, int>& b)
 {
