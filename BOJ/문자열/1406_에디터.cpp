0ce58efb9e93a8b9ec7360a121cdbe9916fb80c2
dldlstjq <inseob135@gmail.com>
2021-01-25 16:13:35 +0900

    boj

--- BOJ/문자열/1406_에디터.cpp
+++ BOJ/문자열/1406_에디터.cpp
@@ -61,3 +61,82 @@ int main() {
 
     return 0;
 }
+
+
+////////////////////////////////////
+//스택 풀이
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+stack<char> head, tail; //커서 왼쪽, 오른쪽
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++)
+        head.push(s[i]);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        char op;
+        cin >> op;
+        //커서 왼쪽
+        if (op == 'L')
+        {
+            if (!head.empty())
+            {
+                tail.push(head.top());
+                head.pop();
+            }
+        }
+
+        //커서 오른쪽
+        else if (op == 'D')
+        {
+            if (!tail.empty())
+            {
+                head.push(tail.top());
+                tail.pop();
+            }
+        }
+
+        //커서 왼쪽 문자 삭제
+        else if (op == 'B')
+        {
+            if (!head.empty())
+                head.pop();
+        }
+
+        //커서 왼쪽에 문자 추가
+        else
+        {
+            char c;
+            cin >> c;
+            head.push(c);
+        }
+    }
+
+    while (!head.empty())
+    {
+        tail.push(head.top());
+        head.pop();
+    }
+
+    string result;
+    while (!tail.empty())
+    {
+        result += tail.top();
+        tail.pop();
+    }
+    cout << result << "\n";
+    return 0;
+}
---
+++ BOJ/스택, 큐, 덱, 리스트, 배열/2840_행운의 바퀴.cpp
@@ -0,0 +1,71 @@
+﻿/*
+예외처리에 신경을 많이 써야 한다.
+우선 인덱스 움직이는 걸 조금 복잡하게 구현했었고 예외처리도 다소 복잡했다.
+그래서 다른 사람의 코드를 찾아봐서 인덱스 조절을 더 간단하게 바꿨고
+중복을 제일 먼저 검사한 다음 중복인 경우 같은 자리에 같은 문자면 상관없지만
+그 자리에 다른 문자가 있으면 실패인 것으로 했다.
+중복이 아닌 경우 비어있으면 문자를 넣고 다른 문자가 있으면 실패로 처리했다.
+*/
+
+#include <iostream>
+using namespace std;
+
+int n, k;
+char wheel[27];
+bool visited[26];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; ++i)
+		wheel[i] = '?';
+
+	int mark = 0;	//화살표
+	while (k--) {
+		int s;
+		char word;
+
+		cin >> s >> word;
+
+		//화살표 이동
+		s %= n;
+		if (mark - s < 0)
+			mark = (mark - s) + n;
+		else
+			mark -= s;
+
+		//중복 확인.
+		if (!visited[word - 'A']) {
+			//칸이 비었을 때
+			if (wheel[mark] == '?') {
+				wheel[mark] = word;
+				visited[word - 'A'] = true;
+			}
+
+			//이미 같은 글자가 있으면 상관 없음
+			else if (wheel[mark] != '?' && wheel[mark] == word) {
+				continue;
+			}
+
+			//해당 칸에 다른 글자가 이미 있으면 실패
+			else if (wheel[mark] != '?' && wheel[mark] != word) {
+				cout << "!";
+				return 0;
+			}
+		}
+		//중복 문자일때 같은 위치에 같은 문자가 아니라면 실패
+		else {
+			if (wheel[mark] != word) {
+				cout << "!";
+				return 0;
+			}
+		}
+	}
+	//출력
+	for (int i = 0; i < n; ++i)
+		cout << wheel[(i + mark) % n];
+	return 0;
+}
