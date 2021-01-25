280d99a9c21f4ae1c940b78143b9396dd6de3748
dldlstjq <inseob135@gmail.com>
2021-01-25 23:03:04 +0900

    boj

---
+++ BOJ/스택, 큐, 덱, 리스트, 배열/3190_뱀.cpp
@@ -0,0 +1,83 @@
+﻿/*
+우선 문제 이해부터 제대로 하지 못했다.
+0초부터 시작하여 X초가 되면 방향을 바꾸는 것인데
+X초 만큼 움직이면 다시 0초부터 그 다음 X초동안 움직이는 걸로 이해해 에제의 답을 도통 이해할 수 없었다.
+
+처음 접근했던 방법은
+무식하게 뱀의 머리와 꼬리의 좌표를 저장하고
+움직이는 만큼 머리와 꼬리를 이동 시킨후 뱀의 몸통은 -1로 보드에 표현했다.
+하지만 뱀의 몸통 전체를 움직이게 하는데 한계가 있었고 결국 해결할 수 없었다.
+다른 사람들은 덱을 이용해서 문제를 해결했다.
+*/
+
+#include <iostream> 
+#include <vector> 
+#include <string> 
+#include <deque>
+using namespace std;
+
+const int MAX = 100; 
+typedef struct { 
+	int y, x; 
+}Dir; 
+Dir moveDir[4] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} }; //E S W N
+
+int N, K, L; 
+bool visited[MAX][MAX];
+bool apple[MAX][MAX];
+
+int main(void) { 
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+	cin >> N; 
+	cin >> K; 
+	for (int k = 0; k < K; k++) { 
+		int y, x; 
+		cin >> y >> x;
+		apple[y - 1][x - 1] = true; 
+	} 
+	cin >> L; 
+	deque<pair<int, char>> turn;
+	for (int l = 0; l < L; l++) { 
+		int X;
+		char C;
+		cin >> X >> C;
+		turn.push_back({ X, C }); 
+	} 
+
+	int sec = 0;
+	int idx = 0; //초기 오른쪽 
+	deque<pair<int, int>> snake; 
+	snake.push_back({ 0, 0 }); //초기 위치
+	visited[0][0] = true;
+
+	while (1) { 
+		sec++; 
+		pair<int, int> before = snake.back();
+		pair<int, int> cur = {before.first + moveDir[idx].y, before.second + moveDir[idx].x };
+		//뱀의 머리가 몸에 부딪히거나, 범위를 벗어날 경우 
+		if (visited[cur.first][cur.second] || !(0 <= cur.first && cur.first < N && 0 <= cur.second && cur.second < N)) { 
+			cout << sec << "\n"; break;
+		} 
+
+		visited[cur.first][cur.second] = true; 
+		snake.push_back(cur); 
+		//사과가 있는 위치면 꼬리 그대로 
+		if (apple[cur.first][cur.second]) 
+			apple[cur.first][cur.second] = false;
+		//사과 없으면 꼬리가 있던 칸을 지운다 
+		else {
+			pair<int, int> tail = snake.front();
+			visited[tail.first][tail.second] = false; 
+			snake.pop_front(); 
+		}
+
+		//마지막에 방향 전환
+		if (turn.size() && turn.front().first == sec) {
+			if (turn.front().second == 'L') 
+				idx = (idx + 3) % 4;
+			else idx = (idx + 1) % 4; turn.pop_front(); 
+		} 
+	} 
+	return 0;
+}
---
+++ BOJ/스택, 큐, 덱, 리스트, 배열/5397_키로거.cpp
@@ -0,0 +1,77 @@
+﻿/*
+1406_에디터에서 스택풀이를 따라하려다 시간이 오래 걸렸다.
+에디터 문제에서 스택에 미리 문자를 삽입한 상태에서 시작한 것을 보고
+이 문제에서도 문자열을 미리 스택에 삽입한 채로 시작하려다 많이 헤맸다.
+에디터의 경우 끝에서 시작하기 때문에 스택에 미리 넣어도 상관없었지만
+여기서는 문자열 맨 처음에서 시작하기 때문에 스택에 넣으면 애로사항이 많았다.
+따라서 문자열 처음부터 반복해서 조건에 맞게 스택에 넣고 빼면 된다.
+
+알고리즘
+1. 결과를 저장하는 result와 화살표 입력시 필요한 temp 스택을 각각 정의합니다.
+2. 왼쪽 화살표를 입력하는 경우 result의 top에 있는 문자를 temp에 push하고 result에서 pop합니다.
+3. 오른쪽 화살표를 입력하는 경우 temp의 top에 있는 문자를 result에 push하고 temp에서 pop합니다.
+4. 백스페이스를 하는 경우 단순히 result에서 한번 pop합니다.
+5. 2, 3, 4번 외에는 단순히 result에 push 합니다.
+6. 반복문을 탈출했을 때, temp에 여전히 문자들이 남아있을 수 있습니다. 따라서 temp에 있는 문자들을 전부 result에 push합니다.
+7. stack은 LIFO(Last In First Out) 구조이기 때문에 거꾸로 출력을 해줘야합니다!
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		string s;
+		stack<char> ret;	//결과 저장
+		stack<char> tmp;	//화살표 임시 저장
+
+		cin >> s;
+		for (int i = 0; i < s.length(); ++i) {
+			//백스페이스면 ret에서 한개 삭제
+			if (s[i] == '-') {
+				if (!ret.empty())
+					ret.pop();
+			}
+			//왼쪽 이동이면 tmp에 있는 문자를 ret으로 이동
+			else if (s[i] == '<') {
+				if (!ret.empty()) {
+					tmp.push(ret.top());
+					ret.pop();
+				}
+			}
+			//오른쪽 이동이면 ret에 있는 문자를 tmp으로 이동
+			else if (s[i] == '>') {
+				if (!tmp.empty()) {
+					ret.push(tmp.top());
+					tmp.pop();
+				}
+			}
+			else {
+				ret.push(s[i]);
+			}
+		}
+
+		//tmp에 있는걸 ret으로 모두 이동
+		while (!tmp.empty()) {
+			ret.push(tmp.top());
+			tmp.pop();
+		}
+		//스택에 저장된 ret을 거꾸로 출력
+		string ans;
+		while (!ret.empty()) {
+			ans += ret.top();
+			ret.pop();
+		}
+		reverse(ans.begin(), ans.end());
+		cout << ans << "\n";
+	}
+	return 0;
+}
