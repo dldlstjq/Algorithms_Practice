4bfae0c2662c66c88a7e805b6adc8be9950a7d72
dldlstjq <inseob135@gmail.com>
2021-01-27 00:18:13 +0900

    boj

---
+++ BOJ/문자열/5076_Web Pages.cpp
@@ -0,0 +1,76 @@
+﻿/*
+스택을 활용하는 문제지만 문자열 파싱이 관건이라서 이걸 못하면 풀 수 없다.
+풀이를 보고 c++에서 공백 기준으로 문자열 파싱 하는 법을 대충 알 수 있었다.
+다음에 한 번 꼭 풀어볼 문제이다.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+using namespace std;
+
+
+void parse(string s) {
+	stack<string> st;
+
+	int pos = 0;
+	//pos가 <아닐때 까지 반복.
+	//<를 찾지 못하면 string::npos를 반환한다.
+	while (s.find("<", pos) != string::npos) {
+		//태그 따기
+		int start_pos = s.find("<", pos);
+		int end_pos = s.find(">", pos);
+		string tag = s.substr(start_pos + 1, end_pos - start_pos - 1);
+
+		//공백으로 split해서 vector 집어넣음
+		//<br /> 같은 형태를 확인하기 위함
+		vector<string> tag_v;
+		//0~255글자 까지 입력 가능하다
+		char str_buff[255];
+		strcpy(str_buff, tag.c_str());
+		char* tok = strtok(str_buff, " ");
+
+		while (tok != nullptr) {
+			tag_v.push_back(string(tok));
+			tok = strtok(nullptr, " ");
+		}
+
+		if (tag_v.back() == "/") {}	//마지막 원소가 /면 아무것도 안함
+		//처음 단어가 /일 때 스택 탑에 같은 단어가 있는지 확인
+		else if (tag_v[0][0] == '/') {
+			if (!st.empty() && st.top() == tag_v[0].substr(1, tag_v[0].length())) {
+				st.pop();
+			}
+			else {
+				cout << "illegal" << "\n";
+				return;
+			}
+		}
+		//시작 태그일때 스택에 푸시
+		else
+			st.push(tag_v[0]);
+		pos = end_pos + 1;
+	}
+
+	if (!st.empty())
+		cout << "illegal" << "\n";
+	else
+		cout << "legal" << "\n";
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string text;
+	while (getline(cin, text)) {
+		if (text == "#")
+			break;
+		parse(text);
+	}
+	return 0;
+}
---
+++ BOJ/스택, 큐, 덱, 리스트, 배열/2304_창고 다각형.cpp
@@ -0,0 +1,67 @@
+﻿/*
+종만북의 울타리 자르기 문제를 스택을 이용한 풀이와 비슷하게 생각해
+해당 코드를 변형시켜 풀어야 한다고 생각해 시간이 오래 걸렸다.
+
+단순히 가장 큰 기둥을 찾고 그걸 기준으로 왼쪽, 오른쪽 높이를 더해가면 됐는데
+너무 복잡하게 생각해 코드도 복잡해졌고 일관성이 없어 해결하지 못했다.
+
+너무 책의 코드에 집착하지 않고 단순히 생각나는대로 해보는 것도 괜찮을 것 같다는 생각이 들었다.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+int n;
+int first = 1001, last = 0;    //첫번째, 마지막 기둥 위치
+int longest;    //가장 큰 기둥 위치
+int h[1001];    //기둥 높이
+
+int solveStack() {
+	stack<int> st;
+	int ret = 0;
+	//시작부터 가장 큰 기둥 위치까지 넓이 계산
+	for (int i = first; i <= longest; ++i) {
+		if (h[i]) {
+			if (st.empty())
+				st.push(h[i]);
+			else if (h[i] > st.top())
+				st.push(h[i]);
+		}
+		ret += st.top();
+	}
+	while (!st.empty())
+		st.pop();
+
+	//끝부터 가장 큰 기둥 전까지 계산
+	for (int i = last; i > longest; --i) {
+		if (h[i]) {
+			if (st.empty())
+				st.push(h[i]);
+			else if (h[i] > st.top())
+				st.push(h[i]);
+		}
+		ret += st.top();
+	}
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		int pos, height;
+		cin >> pos >> height;
+		h[pos] = height;
+		first = min(first, pos);	//시작 기둥 위치
+		last = max(last, pos);		//끝 기둥 위치
+		if (height > h[longest])	//가장 큰 기둥 위치
+			longest = pos;
+	}
+
+	cout << solveStack();
+	return 0;
+}
