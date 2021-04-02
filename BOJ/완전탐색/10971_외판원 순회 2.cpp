27c7d2392e7e6555b0f1bd22f5190c94046be637
Inseob Lee <inseob111@naver.com>
2021-04-02 12:21:52 +0900

    다시 풀기

--- BOJ/완전탐색/10971_외판원 순회 2.cpp
+++ BOJ/완전탐색/10971_외판원 순회 2.cpp
@@ -2,6 +2,12 @@
 유명한 TSP문제이다. 알고리즘 시간에 배웠던게 잠깐 생각났다.
 아예 처음부터 내가 짜기에는 내용을 몰라 인터넷을 참고해서 풀었는데 틀렸다.
 이 역시 순열을 이용하여 모든 정점마다 확인해서 제일 최소 비용을 찾으면 된다.
+
+2021.04.02 
+백트래킹을 이용해 다시 풀었다. 40%에서 틀렸는데 다른 사람의 질문을 보고 해결했다.
+방문한 도시가 N개 일 때 다시 처음으로 돌아오는 과정에서 해당 경로가 연결되었는지도 확인해야한다.
+if문에서 방문 도시가 N개일때만 설정해서 리턴하니 틀렸다.
+
 */
 
 /*
@@ -65,54 +71,62 @@ int main() {
 
 ///////////////////////////////////////////////////
 /*
-처음 인터넷으로 찾은 코드. 정점 0부터 시작하기 때문에 오답이었다. 
-다른 정점도 하게 하려했으나 아직까지는 구현 못함
+백트래킹 이용한 풀이.
 */
 #include <iostream>
-#include <cmath>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
-//가중치 저장 위한 배열
-int dist[10][10];
-int n;
-int TSP(vector<int> path, vector<bool> visited, int len) {
-    //모든 도시 다 방문했을 경우
-    if (path.size() == n)
-        return len + dist[path.back()][0];
-    int ret = 987654321;
 
-    for (int next = 0; next < n; next++) {
-        //방문했다면 패스
-        if (visited[next] == true)
-            continue;
+const int MAX = 10 + 1;
+int N;
+int W[MAX][MAX];
+bool visited[MAX];
 
-        int cur = path.back();
-        path.push_back(next);
-        visited[next] = true;
-        ret = min(ret, TSP(path, visited, len + dist[cur][next]));
-        visited[next] = false;
-        path.pop_back();
+int ret = 987654321;
+void solve(int start, int end, int city_cnt, int cost) {
+    //도시 수가 N이고 처음으로 다시 돌아올 수 있으면 진행
+    //이 부분에서 N만 확인하면 틀림
+    if (city_cnt == N && W[start][end] != 0) {
+        cost += W[start][end];
+        ret = min(ret, cost);
+        return;
     }
-    return ret;
-}
 
+    //start부터 시작해서 방문 안한 도시 방문.
+    for (int k = 0; k < N; ++k) {
+        if (W[start][k] != 0 && !visited[k]) {
+            visited[k] = true;
+            solve(k, end, city_cnt + 1, cost + W[start][k]);
+            visited[k] = false;
+        }
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> dist[i][j];
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            cin >> W[i][j];
 
-    vector<int> path(1, 0);     //경로를 저장할 벡터, 시작 도시 0번도시 선택
-    vector<bool> visited(n, false);     //방문 여부 저장할 벡터. false로 초기화
-    visited[0] = true;      //출발 도시 방문 여부 체크
-    double ret = TSP(path, visited, 0);
+    //i번째 도시부터 시작해서 하나씩 모두 탐색한다.
+    for (int i = 0; i < N; ++i) {
+        visited[i] = true;
+        for (int j = 0; j < N; ++j) {
+            //j번째 도시가 방문 안했고 연결되어 있으면 방문
+            if (W[i][j] != 0 && !visited[j]) {
+                visited[j] = true;
+                //시작은 j번째 도시, 돌아올 도시는 i, 방문한 도시는 2개, 비용은 W[i][j]
+                solve(j, i, 2, W[i][j]);
+                visited[j] = false;
+            }
+        }
+        visited[i] = false;
+    }
+
     cout << ret;
-
     return 0;
 }
