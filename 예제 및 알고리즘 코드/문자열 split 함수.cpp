01e23dbe3ff979f9b64ac34696978434f0413637
Inseob Lee <inseob111@naver.com>
2021-04-13 16:05:05 +0900

    문자열

---
+++ 예제 및 알고리즘 코드/문자열 split 함수.cpp
@@ -0,0 +1,12 @@
+﻿vector<string> split(string str, string delimeter) {
+    vector<string> ret;
+    string tmp = "";
+    for (auto s : str) {
+        if (s == delimeter) {
+            ret.push_back(tmp);
+            tmp = "", continue;
+        }
+        tmp += s;
+    }
+    return ret;
+}
