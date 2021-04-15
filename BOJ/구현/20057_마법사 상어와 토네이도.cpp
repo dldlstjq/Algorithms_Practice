5c7ac1b9acaad95c3934fd618d3e6f592e0ae51e
Inseob Lee <inseob111@naver.com>
2021-04-15 17:41:34 +0900

    다시 풀기

---
+++ BOJ/구현/20057_마법사 상어와 토네이도.cpp
@@ -0,0 +1,125 @@
+﻿/*
+문제이해는 쉽게 됐는데 구현이 어려웠다.
+
+토네이도의 경우 서남동북 순으로 움직이기 때문에 visited 배열을 선언해서 
+한칸 움직일때마다 visited 표시해주고 다음 방향으로 이동할 수 있는지 확인한다.
+이동할 수 있으면 해당 칸으로 방향을 바꿔 이동하고 그렇지 않으면 이동뱡향으로 계속 이동한다.
+
+문제는 모래의 양을 구하는 것이었다. 1%, 2%, 7%, 10% 2개에 5% 1개, a까지 고려하면 전체 10개의 경우의 수가 나온다.
+이 경우의수가 전체 4방향이므로 총 40가지 경우를 고려해야 하는데 이걸 전부 다 일일이 구현하는건 코드가 너무 복잡해지고
+어려울 것 같았다.
+
+상대적인 좌표를 딱 정하면 좋겠다 생각했는데 로직이 생각나지 않아 못풀었다.
+
+퍼센트는 퍼센트대로 정의하고 각 방향에 맞게 2차원 배열을 해서 좌표를 설정하면 된다.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 1000 + 1;
+int N;
+int A[MAX][MAX];
+bool visited[MAX][MAX];
+
+int dy[4] = { 0,1,0,-1 };
+int dx[4] = { -1,0,1,0 };
+
+const int blow_y[4][10] = {
+   {-1,1,-2,-1,1,2,-1,1,0,0},
+    {-1,-1,0,0,0,0,1,1,2,1},
+    {-1,1,-2,-1,1,2,-1,1,0,0},
+    {1,1,0,0,0,0,-1,-1,-2,-1}
+};
+
+const int blow_x[4][10] = {
+   {1,1,0,0,0,0,-1,-1,-2,-1},
+    {-1,1,-2,-1,1,2,-1,1,0,0},
+    {-1,-1,0,0,0,0,1,1,2,1},
+    {-1,1,-2,-1,1,2,-1,1,0,0}
+};
+const int percent[9] = { 1,1,2,7,7,2,10,10,5 };
+
+int ret;
+
+bool is_in(int y, int x) {
+    return 0 <= y && y < N && 0 <= x && x < N;
+}
+
+int blow_sand(int y, int x, int d) {
+    int sum = 0, init = A[y][x];
+    for (int i = 0; i < 10; ++i) {
+        int sand = 0;
+        //마지막은 a로 이동하는 모래이다.
+        if (i != 9) {
+            sand = init * percent[i] / 100;
+            A[y][x] -= sand;
+        }
+        else
+            sand = A[y][x];
+
+        //모래가 날린 부분이 범위 안이면 더해주고 아니면 sum에 저장
+        int ny = y + blow_y[d % 4][i];
+        int nx = x + blow_x[d % 4][i];
+
+        if (!is_in(ny, nx)) {
+            sum += sand;
+            continue;
+        }
+
+        A[ny][nx] += sand;
+
+    }
+    //현재 좌표의 모래는 없어진다.
+    A[y][x] = 0;
+    return sum;
+}
+
+void solve(int y, int x) {
+    //토네이도 이동
+    int dir = 0;
+
+    while (1) {
+        y += dy[dir % 4];
+        x += dx[dir % 4];
+
+        if (y == 0 && x == -1)
+            break;
+
+        visited[y][x] = true;
+
+        ret += blow_sand(y, x, dir);
+
+        int ny = y + dy[(dir + 1) % 4];
+        int nx = x + dx[(dir + 1) % 4];
+
+        if (!visited[ny][nx])
+            dir += 1;
+
+    }
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            cin >> A[i][j];
+
+    int start_y = N / 2;
+    int start_x = N / 2;
+
+    visited[start_y][start_x] = true;
+    solve(start_y, start_x);
+
+    cout << ret;
+
+    return 0;
+}
---
+++ BOJ/구현/20058_마법사 상어와 파이어스톰.cpp
@@ -0,0 +1,165 @@
+﻿/*
+문제를 잘못 이해해서 시간이 많이 걸렸다.
+
+격자를 회전하는 경우 모든 칸에 대해 회전을 하면 되는데
+예제에 색깔이 있는 부분만 회전을 해야 하는 줄 알고 그 부분을 구현하는 것이 힘들었다.
+분할정복을 써서 들어가는데 어디서 어떻게 조건을 걸어야 색깔 있는 부분만 회전할 수 있는지
+몰라서 해결하지 못했다.
+
+다른 사람 풀이를 보니 애초에 모든 2^L x 2^L 격자에 대해 회전을 하면 됐다.
+문제를 제대로 안보고 대충 해석한게 가장 큰 실수인 것 같다.
+
+모든 격자에 대해 회전을 해야 하는 걸 알고나니 회전은 그렇게 오래 걸리지 않았다.
+격자의 행이 가장 오른쪽 열로 이동하는 것이어서 비교적 쉽게 구현했다.
+
+얼음이 줄어드는 조건도 다소 난해했는데 인접한 얼음이 3개 미만이면 해당 얼음을 1 감소시키면 된다.
+다만 여기서도 모든 얼음이 동시에 감소하기 때문에 좌표하나를 검사해서 순서대로 1씩 감소하면
+제대로 된 답이 나오지 않았다.
+
+얼음 감소까지 하고 전체 얼음 갯수는 간단히 구하고 얼음 덩어리는 dfs를 돌려 구했다.
+
+문제를 천천히 잘 읽어서 뭘 구현해야 하는지 빨리 파악하는게 제일 중요한것 같다.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 64 + 1;
+int N, Q, L;
+int A[MAX][MAX];
+int copy_A[MAX][MAX];
+
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+
+bool visited[MAX][MAX];
+int cnt;
+
+void copy() {
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            copy_A[i][j] = A[i][j];
+}
+
+
+//회전
+void rotate(int len, int y, int x) {
+    copy();
+    //각 행은 맨 오른쪽 열로 이동한다.
+    //한 행이 끝나면 열을 1씩 감소시킨다.
+    int c = x + len - 1;
+    for (int i = y; i < y + len; ++i) {
+        int r = y;
+        for (int j = x; j < x + len; ++j) {
+            A[r++][c] = copy_A[i][j];
+        }
+        c--;
+    }
+}
+
+bool check_adjacent(int y, int x) {
+    //얼음이 있는 칸 3개 또는 그 이상과 인접해있지 않은 칸은 얼음 양 1 줄어든다.
+    int ice_cnt = 0;
+    for (int d = 0; d < 4; ++d) {
+        int ny = y + dy[d];
+        int nx = x + dx[d];
+
+        if (0 <= ny && ny < N && 0 <= nx && nx < N) {
+            if (A[ny][nx] != 0)
+                ice_cnt++;
+        }
+    }
+
+    if (ice_cnt < 3)
+        return false;
+    else
+        return true;
+}
+
+//얼음 감소. 동시에 감소하기 때문에 하나씩 검사해서 감소를 시켜버리면 
+//인접한 얼음에도 영향을 미친다. 따라서 인접 얼음이 3개미만인 것들을 모아 한번에 처리해야 한다.
+void ice_decrease() {
+    vector<pair<int, int>> v;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            //얼음이 없는 칸은 패스
+            if (A[i][j] == 0)
+                continue;
+            //인접한 얼음 3개미만인 좌표 따로 저장
+            if (!check_adjacent(i, j)) {
+                v.push_back({ i,j });
+                //A[i][j] -= 1;
+            }
+
+
+        }
+    }
+
+    //한꺼번에 감소
+    for (int i = 0; i < v.size(); ++i) {
+        A[v[i].first][v[i].second] -= 1;
+    }
+
+}
+
+void ice_biggest(int y, int x) {
+    visited[y][x] = true;
+    cnt++;
+    for (int i = 0; i < 4; ++i) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (0 <= ny && ny < N && 0 <= nx && nx < N) {
+            if (A[ny][nx] != 0 && !visited[ny][nx])
+                ice_biggest(ny, nx);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> Q;
+    N = 1 << N;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cin >> A[i][j];
+        }
+    }
+
+    for (int i = 0; i < Q; ++i) {
+        cin >> L;
+        L = 1 << L;
+        for (int i = 0; i < N; i += L)
+            for (int j = 0; j < N; j += L)
+                rotate(L, i, j);
+
+        //얼음 감소
+        ice_decrease();
+
+    }
+
+    //남아있는 얼음 합
+    int sum = 0;
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            sum += A[i][j];
+
+    //가장 큰 덩어리
+    int ret = 0;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cnt = 0;
+            if (A[i][j] != 0 && !visited[i][j]) {
+                ice_biggest(i, j);
+                ret = max(ret, cnt);
+            }
+        }
+    }
+
+    cout << sum << "\n" << ret;
+    return 0;
+}
