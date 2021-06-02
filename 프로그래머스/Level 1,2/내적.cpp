972f7a7662499d324435953841fec92619c263f5
Inseob Lee <inseob111@naver.com>
2021-06-02 17:29:55 +0900

    programmers

---
+++ 프로그래머스/Level 1,2/내적.cpp
@@ -0,0 +1,17 @@
+﻿/*
+문제 대로 구하면 끝이다.
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 1234567890;
+    answer = 0;
+    for (int i = 0; i < a.size(); ++i) {
+        answer += (a[i] * b[i]);
+    }
+    return answer;
+}
