baef0f09debde334b6d343014e585b9975310614
Inseob Lee <inseob111@naver.com>
2021-06-09 22:51:13 +0900

    다시 풀기

--- BOJ/완전탐색/1339_단어 수학.cpp
+++ BOJ/완전탐색/1339_단어 수학.cpp
@@ -15,6 +15,10 @@ C   B  A
 순열이라 적혀있어 순열로만 풀이를 생각한게 못푼 원인이지 않나 싶다.
 */
 
+// 2021.06.09 완전탐색으로 생각했는데 풀이가 생각나지 않았다.
+// 어떤 알파벳이 나왔는지, 각 알파벳에 대응하는 숫자는 뭘 해야 하는지 고려해야할게 많다.
+// 그리디 방식으로 푸는 것이 가장 간단하고 완전탐색으로도 가능한데 조금 복잡하다.
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -70,4 +74,79 @@ int main() {
 
     cout << ret;
     return 0;
+}
+
+
+
+// 완전 탐색
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+vector<string> word;
+int result = -1;
+vector<char> alpha;				//들어있는 알파벳
+bool type[26] = { false, };		//어떤 알파벳이 사용되었는지 확인
+bool check[10];
+vector<int> v;			//각 알파벳에 해당하는 숫자
+
+void cal() {
+	int t_result = 0;
+	int cnt = 0;
+	int a[26];
+	for (int i = 0; i < alpha.size(); i++) {
+		char c = alpha[i];
+		int num = v[i];
+		a[c - 'A'] = num;
+	}
+	for (int i = 0; i < word.size(); i++) {
+		int temp = 0;
+		for (int j = 0; j < word[i].size(); j++) {
+			temp *= 10;
+			temp += a[word[i][j] - 'A'];
+		}
+		t_result += temp;
+	}
+	result = max(result, t_result);
+}
+
+void dfs(int cnt) {
+	if (cnt == 10) {
+		cal();
+		return;
+	}
+	for (int i = 10 - alpha.size(); i < 10; i++) {
+		if (!check[i]) {
+			v.push_back(i);
+			check[i] = true;
+			dfs(cnt + 1);
+			check[i] = false;
+			v.pop_back();
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int num;
+	string ss;
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		cin >> ss;
+		word.push_back(ss);
+		for (int j = 0; j < ss.size(); j++) {
+			int c = ss[j] - 'A';
+			if (!type[c]) {
+				type[c] = true;
+				alpha.push_back(ss[j]);
+			}
+		}
+	}
+
+	//가장 큰 수부터 시작하기 위해 10-alpha.size()를 넣어준다.
+	dfs(10 - alpha.size());
+	cout << result;
+	system("pause");
+	return 0;
 }
