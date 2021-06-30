9e38f850064057a6afd407c25fa77c09037f3f26
Inseob Lee <inseob111@naver.com>
2021-07-01 00:48:01 +0900

    다시 풀기

--- BOJ/완전탐색/1963_소수 경로.cpp
+++ BOJ/완전탐색/1963_소수 경로.cpp
@@ -4,6 +4,104 @@
 각 자리수를 하나씩 바꾸는 것에서 해결하는데 어려움을 겪었다.
 두번째로 풀때는 dist 배열을 안쓰고 큐에 pair를 써서 해결했다.
 */
+
+/*
+2021.06.30 bfs를 이용해서 풀이했는데 visited 개념을 자꾸 잊어먹어 제대로 된 답이 안나왔다.
+한 번 계산한 것은 다시 계산하지 않아야 bfs가 종료되는데 그걸 하지 않으니 계속 무한루프에 빠졌다.
+그래도 복습을 하면서 완전탐색 감을 조금씩 찾아나가는 것 같다.
+*/
+
+// 복습 코드
+#include <iostream> 
+#include <cstring> 
+#include <cmath> 
+#include <algorithm>
+#include <queue>
+#include <string>
+using namespace std;
+
+const int MAX = 9999 + 1;
+int A, B;
+bool prime[MAX];
+bool visited[MAX];
+
+void eratosthenes() {
+    memset(prime, 1, sizeof(prime));
+    prime[0] = prime[1] = false;
+    int sqrtn = int(sqrt(MAX));
+    for (int i = 2; i <= sqrtn; ++i) {
+        if (prime[i]) {
+            for (int j = i * i; j < MAX; j += i)
+                prime[j] = false;
+        }
+    }
+}
+
+
+int main(int argc, char** argv) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    eratosthenes();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        memset(visited, false, sizeof(visited));
+        cin >> A >> B;
+        int ret = -1;
+        queue<pair<int, int>> q;
+        q.push({ A,0 });
+        visited[A] = true;
+
+        while (!q.empty()) {
+            int now = q.front().first;
+            int cnt = q.front().second;
+            q.pop();
+
+            if (now == B) {
+                ret = cnt;
+                break;
+            }
+
+
+            // 첫번째 자리는 1부터 시작
+            for (int i = 0; i < 4; ++i) {
+                // 한 번에 한자리 밖에 못바꾼다. 한자리씩 다 바꿔서 소수이면 ok.
+                string s = to_string(now);
+                for (int j = 0; j <= 9; ++j) {
+                    //맨 앞자리는 0이 올 수 없다.
+                    if (i == 0 && j == 0)
+                        continue;
+
+                    // 같은 숫자면 넘어가고 다른 숫자일때 소수이면 푸시
+                    if (s[i] - '0' == j)
+                        continue;
+                    s[i] = j + '0';
+                    if (prime[stoi(s)] && !visited[stoi(s)]) {
+                        q.push({ stoi(s), cnt + 1 });
+                        visited[stoi(s)] = true;
+                    }
+                }
+            }
+
+
+        }
+
+        if (ret != -1)
+            cout << ret << '\n';
+        else
+            cout << "Impossible" << '\n';
+    }
+
+    return 0;
+}
+
+
+////////////////////////////////////////////////////
+
 #include <iostream>
 #include <cstring>
 #include <string>
