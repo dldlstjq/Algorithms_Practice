4aaa08c4ca714d3fbe6f3a21cc97076d1abe7646
Inseob Lee <inseob111@naver.com>
2021-07-01 20:28:08 +0900

    boj

--- BOJ/완전탐색/2231_분해합.cpp
+++ BOJ/완전탐색/2231_분해합.cpp
@@ -5,6 +5,54 @@
 i가 n보다 크거나 같으면 생성자가 없기때문에 종료한다.
 */
 
+// 2021.07.01 1부터 끝까지 다 해보면 된다. 배열을 따로 선언할 필요가 없는데 선언해서 메모리만 잡아먹었다;;
+// 문자열로 바꿔 계산해도 되지만 숫자인 상태에서 모듈러와 나머지 연산을 이용하는게 더 깔끔하다.
+#include <iostream> 
+using namespace std;
+
+const int MAX = 1000000 + 1;
+int N;
+int arr[MAX];
+
+int calc(int num) {
+	int sum = num;
+
+	while (num > 0) {
+		sum += num % 10;
+		num /= 10;
+	}
+	return sum;
+}
+
+int main(int argc, char** argv) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N;
+
+	int ans = -1;
+
+	for (int i = 1; i <= N; ++i) {
+		arr[i] = calc(i);
+
+		if (arr[i] == N) {
+			ans = i;
+			break;
+		}
+
+	}
+
+	if (ans != -1)
+		cout << ans;
+	else
+		cout << 0;
+
+	return 0;
+}
+
+
+/////////////////////////////////
 #include <iostream>
 #include <vector>
 #include <algorithm>
