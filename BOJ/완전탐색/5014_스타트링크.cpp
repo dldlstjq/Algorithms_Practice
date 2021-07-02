b219e6f70ab91461b4108d62f59c78fd6602c2d3
Inseob Lee <inseob111@naver.com>
2021-07-02 23:53:13 +0900

    boj

--- BOJ/완전탐색/5014_스타트링크.cpp
+++ BOJ/완전탐색/5014_스타트링크.cpp
@@ -2,6 +2,72 @@
 BFS를 이용하면 쉽게 풀 수 있었다. 
 현재 위치랑 버튼 누르는 횟수를 페어로 큐에 넣어서 U,D층을 다 검사해서 G층에 도착할 때 버튼 횟수를 출력하면 된다.
 */
+
+
+// 2021.07.02 bfs로 쉽게 해결했다.
+
+#include <iostream> 
+#include <queue>
+using namespace std;
+
+const int MAX = 1000000 + 1;
+int F, S, G, U, D;
+int building[MAX];
+bool visited[MAX] = { false };
+
+
+int main(int argc, char** argv) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> F >> S >> G >> U >> D;
+
+    queue<pair<int, int>> q;
+    q.push({ S,0 });
+    visited[S] = true;
+
+    int ret = -1;
+    while (!q.empty()) {
+        int now = q.front().first;
+        int cnt = q.front().second;
+        q.pop();
+
+        if (now == G) {
+            ret = cnt;
+            break;
+        }
+
+        int next1 = now + U;
+        int next2 = now - D;
+
+        if (next1 <= F) {
+            if (!visited[next1]) {
+                visited[next1] = true;
+                q.push({ next1,cnt + 1 });
+            }
+        }
+
+        if (next2 > 0) {
+            if (!visited[next2]) {
+                visited[next2] = true;
+                q.push({ next2,cnt + 1 });
+            }
+        }
+
+    }
+
+    if (ret != -1)
+        cout << ret;
+    else
+        cout << "use the stairs";
+
+    return 0;
+}
+
+
+///////////////////////////////////////
+
 #include <iostream>
 #include <cstring>
 #include <vector>
